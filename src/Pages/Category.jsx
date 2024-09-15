be148ca87100cf401aa6dd715330865c284cc1db
Kiirankale <kirankale431@gmail.com>
2024-09-15 23:06:57 +0530

    minor fixes

--- src/Pages/Category.jsx
+++ src/Pages/Category.jsx
@@ -123,7 +123,12 @@ export default function Category() {
           )}
         </>
       ) : (
-        <p>There are no current offers</p>
+        <p>
+        There are no current{" "}
+        {params.categoryName === "rent"
+          ? "places for rent"
+          : "places for sale"}
+      </p>
       )}
     </div>
   );
