0e18ed8ce7643fa66e0097b417bd60eced028311
Kiirankale <kirankale431@gmail.com>
2024-09-15 10:37:52 +0530

    Create slider component to the home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Slider from '../components/Slider'
+
 
 export default function Home() {
+  
   return (
     <div>
-      Home
+      <Slider />
     </div>
   )
 }
